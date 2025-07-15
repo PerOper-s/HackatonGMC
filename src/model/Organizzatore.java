4836959062810921d8a0b202938feedf986113a9
PerOper-s <poisonstatic157@gmail.com>
2025-07-15 16:35:27 +0200

    AggiunteDashboardOrganizzatore
    --

---
+++ src/model/Organizzatore.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Organizzatore extends Utente {
+
+    public Organizzatore(String mail) {
+        super(mail);
+    }
+
+    // Crea un nuovo Hackathon
+    public Hackathon creaHackathon(String titolo, String sede, int maxPartecipanti, int maxGrandezzaTeam) {
+        Hackathon hack = new Hackathon(titolo, sede, this, maxPartecipanti, maxGrandezzaTeam);
+        System.out.println("Hackathon \"" + hack.getTitolo() + "\" creato con successo.");
+        return hack;
+    }
+
+    // Invita i giudici
+    public void invitaGiudici() {
+        // In un caso reale si inviterebbero giudici specifici; qui stampa un messaggio di esempio
+        System.out.println("Giudici invitati all'hackathon.");
+    }
+
+    // Invita uno specifico giudice a un determinato hackathon
+    public void invitaGiudici(Giudice giudice, Hackathon hackathon) {
+        hackathon.aggiungiGiudice(giudice);
+        giudice.partecipaHackathon(hackathon);
+        System.out.println("Giudice " + giudice.getMail() + " invitato all'hackathon \"" + hackathon.getTitolo() + "\"");
+    }
+}
